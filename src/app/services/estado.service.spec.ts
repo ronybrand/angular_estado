4f2c1d10e8c8004e8a715ebc4d405d45d15030e5
Rony Brand <ronybrand@gmail.com>
2026-08-17 11:26:23 -0300

    test(estado.service): usa provideHttpClient/provideHttpClientTesting
    Alinha com os demais specs de interceptor, que já usam a API atual.
    HttpClientTestingModule é a forma antiga/depreciada de configurar o
    TestBed para testes de HTTP.

--- src/app/services/estado.service.spec.ts
+++ src/app/services/estado.service.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 
 import { EstadoService } from './estado.service';
 import { Estado } from '../interfaces/estado';
@@ -13,7 +14,7 @@ describe('EstadoService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     });
     service = TestBed.inject(EstadoService);
     httpMock = TestBed.inject(HttpTestingController);
